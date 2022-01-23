7da1c41459bfa2e3110f5f9c439100527ed606cb
Troll <trino@mail.ru>
2022-01-23 22:50:16 +0300

    Save client information the cool way

--- app.rb
+++ app.rb
@@ -26,13 +26,8 @@ end
 
 post '/visit' do
 
-	@username = params[:username]
-	@phone = params[:phone]
-	@datetime = params[:datetime]
-	@barber = params[:barber]
-	@color = params[:color]
-
-
+	c = Client.new params[:client]
+	c.save
 
 	erb "<h2>Спасибо, вы записались!</h2>"
 
